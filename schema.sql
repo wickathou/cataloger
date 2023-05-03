2c473f969f5cc5bd42d5d7da0eb282fc756a50fd
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-03 14:59:00 +0100

    Add musicAlbum table

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE music (
+    id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    on_spotify BOOLEAN,
+    FOREIGN KEY (id) REFERENCES items(id)
+);
+
+CREATE TABLE genres (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
