3acc225daef03e1159bd70d370e7920a170f307d
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-03 14:34:48 +0100

    Update genre.rb file

--- music/genre.rb
+++ music/genre.rb
@@ -1,14 +1,27 @@
 class Genre
   attr_reader :id, :name, :items
 
-  def initialize(name)
-    @id = securerandom.uuid
-    @name = name
-    @items = []
+  attr_accessor :artist, :on_spotify
+
+  def initialize(name, description, price, artist, on_spotify)
+    super(name, description, price)
+    @artist = artist
+    @on_spotify = on_spotify
   end
 
-  def add_item(item)
-    item.genre = self
-    @items << item unless @items.include?(item)
+  def can_be_archived?
+    super && @on_spotify
+  end
+
+  def to_json(*args)
+    {
+      'artist' => @artist,
+      'on_spotify' => @on_spotify
+    }.to_json(*args)
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    new(data['artist'], data['on_spotify'])
   end
 end
