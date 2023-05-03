7cfe0d6d2e63128c2cf144ccb0b6d60b13b65ace
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-03 14:59:24 +0100

    Add items table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,13 @@
+CREATE TABLE items (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
+
 CREATE TABLE music (
     id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
