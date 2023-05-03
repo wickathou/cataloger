9cfb0f23c971affd0de7b550ab8f3de233b91e95
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-03 14:31:33 +0100

    Update music.rb file

--- music/music.rb
+++ music/music.rb
@@ -1,16 +1,27 @@
 require_relative '/lib/item'
 
-class MusicAlbum < item
-  attr_accessor :on_spotify
+class MusicAlbum < Item
+  attr_accessor :artist, :on_spotify
 
-  def initialize(on_spotify, publish_date = 'unknown')
-    super(publish_date)
+  def initialize(name, description, price, artist, on_spotify)
+    super(name, description, price)
+    @artist = artist
     @on_spotify = on_spotify
   end
 
-  private
-
   def can_be_archived?
     super && @on_spotify
+  end
+
+  def to_json(*args)
+    {
+      'artist' => @artist,
+      'on_spotify' => @on_spotify
+    }.to_json(*args)
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    new(data['artist'], data['on_spotify'])
   end
 end
