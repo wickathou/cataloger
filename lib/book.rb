20ee23d088a95a87f50bd37b9511552e0942c3eb
Javier <javidx@gmail.com>
2023-05-04 15:25:06 +0300

    integrating music and book into app logic

---
+++ lib/book.rb
@@ -0,0 +1,17 @@
+require_relative 'item'
+
+class Book < Item 
+  attr_accessor :publisher, :cover_state
+  def initialize(genre_array, author_array, label_array, publisher, cover_state)
+    super(genre_array, author_array, label_array)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived
+      super || @cover_state == 'bad'
+  end
+end
+
+book = Book.new(['a', 'b', 'c'], [Author.new], ['1','2'], 'publisher', 'cover_state')
+puts book.inspect
