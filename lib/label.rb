18cf6392933230cd1d4603d7956091c07c778c40
Javier <javidx@gmail.com>
2023-05-04 15:38:56 +0300

    Added label into app logic

---
+++ lib/label.rb
@@ -0,0 +1,20 @@
+require_relative 'category_utils'
+require 'securerandom'
+
+class Label
+include Category_Utils
+    attr_accessor :id, :title, :color, :items
+    def initialize
+        @id = SecureRandom.uuid
+        @title = text_input('title')
+        @color = text_input('color')
+        @items = []
+    end
+    def add_item(item)
+        @items << item
+        item.label = self
+    end
+end
+
+label = Label.new
+puts label.inspect
