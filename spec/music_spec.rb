0d36d3dfbe83b747f772601437c819a675e0298d
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-05-05 17:30:43 +0100

    Fix test

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -1,13 +1,11 @@
-require_relative '../music/music'
-
-require_relative '../lib/item'
+require_relative '../lib/music'
 
 describe MusicAlbum do
   before :each do
-    @music = MusicAlbum.new(true)
+    @music = MusicAlbum.new('Album Name', 'Artist Name', 'Genre', true)
   end
 
-  it 'check if the music is on spotify' do
+  it 'checks if the music is on spotify' do
     expect(@music.on_spotify).to eq(true)
   end
 end
